29d2de1f2077fd4e3b7f09f3b3068b88afa866be
BlaBlaHuman <thelastrager29@gmail.com>
2023-05-07 02:47:20 +0200

    Add more checks

--- contract.sol
+++ contract.sol
@@ -49,18 +49,37 @@ contract ETH_exchange {
     }
 
     function sellToken(address _token, uint _amount) tokenSupported(IERC20(_token)) external {
-        require(_amount > 0, "You are trying to sell 0 tokens");
+        require(_amount > 0, "Selling 0 tokens is not allowed");
         
         /*
             To sell tokens user has to allow the contract to spend tokens from their balance
         */
         require(IERC20(_token).allowance(msg.sender, address(this)) >= _amount, "The contract cannot spend the defined amount of tokens from the caller's balance");
-        
-        require(IERC20(_token).transferFrom(msg.sender, address(this), _amount), "Transaction was not successful");
-        require(payable(msg.sender).send(exchangeRate[IERC20(_token)] * _amount), "Transaction was not successful");
+        require(IERC20(_token).transferFrom(msg.sender, address(this), _amount), "Unable to transfer tokens from the seller");
+
+        require(address(this).balance >= exchangeRate[IERC20(_token)] * _amount, "Not enough wei to buy tokens");
+        require(payable(msg.sender).send(exchangeRate[IERC20(_token)] * _amount), "Unable to transfer wei to the seller");
     }
 
     function buyToken(address _token) tokenSupported(IERC20(_token)) external payable {
-        require(IERC20(_token).transferFrom(address(this), msg.sender, msg.value / exchangeRate[IERC20(_token)]), "Transaction was not successful");
+        require(msg.value > 0, "0 wei transfered");
+
+        if (IERC20(_token).balanceOf(address(this)) >= msg.value / exchangeRate[IERC20(_token)]) {
+            /*
+                Case when the contract is able to sell the needed amount of tokens to the buyer
+            */
+            require(IERC20(_token).transferFrom(address(this), msg.sender, msg.value / exchangeRate[IERC20(_token)]), "Unable to transfer tokens to the buyer");
+        }
+        else {
+            /*
+                Case when there are not enough tokens to sell
+                The contract needs to send the change back to the buyer
+            */
+            uint diff = msg.value - IERC20(_token).balanceOf(address(this)) * exchangeRate[IERC20(_token)];
+            require(payable(msg.sender).send(diff), "Unable to pay the change");
+            require(IERC20(_token).transferFrom(address(this), msg.sender, IERC20(_token).balanceOf(address(this))), "Unable to transfer tokens to the buyer");
+
+        }
+
     }
 }
